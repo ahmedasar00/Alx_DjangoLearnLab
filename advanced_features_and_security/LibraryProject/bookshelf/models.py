87c8e4a9bd9fe00753cd384db17ca2726c826eca
Ahmed Asar <a7med3sar22@gmail.com>
2025-08-01 11:48:57 +0300

    Task0

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -8,4 +10,42 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-# Create your models here.
+
+
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields.get('is_superuser'):
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)  # Email is the unique identifier
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'  # Use email for authentication instead of username
+    REQUIRED_FIELDS = ['username']  # Username is still required but not for authentication
+
+    def __str__(self):
+        return self.email
