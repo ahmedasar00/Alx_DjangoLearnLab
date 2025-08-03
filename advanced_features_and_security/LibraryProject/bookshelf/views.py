deecf32ab514d5d348d4a4f5b5d1c7bfca1d1ff9
Ahmed Asar <a7med3sar22@gmail.com>
2025-08-03 19:21:14 +0300

    Document permissions and groups in README.md

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,13 +4,13 @@ from django.contrib.auth.decorators import login_required, permission_required
 from .models import Article
 
 @login_required
-@permission_required('your_app_name.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'article_list.html', {'articles': articles})
 
 @login_required
-@permission_required('your_app_name.can_create', raise_exception=True)
+@permission_required('bookshelf.can_create', raise_exception=True)
 def article_create(request):
     if request.method == "POST":
         title = request.POST.get("title")
@@ -20,7 +20,7 @@ def article_create(request):
     return render(request, 'article_form.html')
 
 @login_required
-@permission_required('your_app_name.can_edit', raise_exception=True)
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def article_edit(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     if request.method == "POST":
@@ -31,7 +31,7 @@ def article_edit(request, article_id):
     return render(request, 'article_form.html', {'article': article})
 
 @login_required
-@permission_required('your_app_name.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def article_delete(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     if request.method == "POST":
