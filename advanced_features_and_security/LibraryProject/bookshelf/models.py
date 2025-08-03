18fca483b870a4f41fd65ff6bdf3eef30653e6a8
Ahmed Asar <a7med3sar22@gmail.com>
2025-08-03 19:13:50 +0300

    Implement permissions and groups for Article model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Book(models.Model):
@@ -8,7 +11,7 @@ class Book(models.Model):
     publication_year = models.IntegerField()
 
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author} ({self.publication_year})"
 
 
 
@@ -49,3 +52,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view article"),
+            ("can_create", "Can create article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
+
+    def __str__(self):
+        return self.title
