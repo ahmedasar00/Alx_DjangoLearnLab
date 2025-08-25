dfbcda6afb4b75438f5bef1856c651a60d86dd1f
Ahmed Asar <a7med3sar22@gmail.com>
2025-08-25 12:17:47 +0300

    Update URL patterns for Following

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,15 +3,14 @@
 from django.urls import path
 from .views import UserViewSet
 
-# Manually define view actions
 user_list = UserViewSet.as_view({"get": "list"})
 user_detail = UserViewSet.as_view({"get": "retrieve"})
 user_follow = UserViewSet.as_view({"post": "follow"})
 user_unfollow = UserViewSet.as_view({"post": "unfollow"})
 
+
 urlpatterns = [
     path("users/", user_list, name="user-list"),
-    # Use <int:user_id> to match the checker and the updated ViewSet
     path("users/<int:user_id>/", user_detail, name="user-detail"),
     path("users/<int:user_id>/follow/", user_follow, name="user-follow"),
     path("users/<int:user_id>/unfollow/", user_unfollow, name="user-unfollow"),
